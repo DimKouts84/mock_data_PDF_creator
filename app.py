ec1620b6fb4b2ecd0badbcf737b8bcc72dc2951d
DimKouts84 <dimitriskouts@gmail.com>
2024-11-09 19:44:59 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   app.py
    	new file:   data.csv
    	new file:   logo.png
    
    Changes not staged for commit:
    	modified:   .gitignore
    	modified:   test_data.csv
    
    Untracked files:
    	LOGO_OR.webp
    	data - backup.csv

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from reportlab.lib.styles import getSampleStyleSheet
 import os
 
 # Load data from CSV
-data = pd.read_csv('test_data.csv')
+data = pd.read_csv('data.csv')
 
 # Path to logo image
 logo_path = 'LOGO.PNG'  # Replace with the path to your logo image
@@ -35,33 +35,32 @@ for index, row in data.iterrows():
         ["Patient Name:", row['Patient Name']],
         ["Gender:", row['Gender']],
         ["Date of Birth:", row['Date of Birth']],
-        ["Order No:", row['Order No']],
-        ["Order Date:", row['Order Date']],
         ["Report Date:", row['Report Date']],
         ["Execution ID:", row['Execution ID']],
         ["Sample Date:", row['Sample Date']]
     ]
     report_table = Table(report_info, colWidths=[2*inch, 3*inch])
     report_table.setStyle(TableStyle([
-        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('BACKGROUND', (0, 0), (-1, 0), colors.lightblue),
         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
         ('FONTNAME', (0,0), (-1,-1), 'Helvetica'),
         ('FONTSIZE', (0,0), (-1,-1), 9),
         ('BOTTOMPADDING', (0, 0), (-1, 0), 9),
-        ('BACKGROUND', (0, 1), (-1, -1), colors.lightgrey),
         ('GRID', (0, 0), (-1, -1), 1, colors.black),
     ]))
 
     # Combine logo and report_info into a single table
     combined_table = Table([[im, report_table]], colWidths=[2*inch, 5*inch])
     combined_table.setStyle(TableStyle([
-        ('VALIGN', (0, 0), (0, 0), 'TOP'),
-        ('VALIGN', (1, 0), (1, 0), 'TOP'),
+        ('VALIGN', (0, 0), (0, 0), 'CENTER'),
+        ('ALIGN', (0, 0), (0, 0), 'CENTER'),
+        ('VALIGN', (1, 0), (1, 0), 'CENTER'),
+        ('ALIGN', (0, 0), (0, 0), 'CENTER')
+        
     ]))
 
     elements.append(combined_table)
-    elements.append(Spacer(0.5, 0.25*inch))
+    elements.append(Spacer(0.5, 0.20*inch))
 
     # Add Examination Information
     exam_info = [
@@ -69,7 +68,7 @@ for index, row in data.iterrows():
     ]
     
     # Add each exam as a row in the exam_info table
-    for i in range(1, 3):  # Assuming up to 5 exams per report, adjust as needed
+    for i in range(1, 7):  # Assuming up to 5 exams per report, adjust as needed
         exam_info.append([
             row[f'LOINC Code {i}'],
             row[f'Exam Name {i}'],
@@ -82,7 +81,7 @@ for index, row in data.iterrows():
     exam_table = Table(exam_info, colWidths=[0.85*inch, 2.1*inch, 0.85*inch, 0.65*inch, 1.50*inch, 1.05*inch])
     exam_table.setStyle(TableStyle([
         ('GRID', (0,0), (-1,-1), 1, colors.black),
-        ('BACKGROUND', (0,0), (-1,0), colors.lightgrey),
+        ('BACKGROUND', (0,0), (-1,0), colors.lightblue),
         ('FONTNAME', (0,0), (-1,-1), 'Helvetica'),
         ('FONTSIZE', (0,0), (-1,-1), 9),
         ('ALIGN', (0,0), (-1,-1), 'LEFT')
